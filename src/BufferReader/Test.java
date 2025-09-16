47557d1028b8cf2d52560eb505c5628a1256765f
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-16 21:54:57 +0600

    update

--- src/BufferReader/Test.java
+++ src/BufferReader/Test.java
@@ -1,11 +1,14 @@
 package BufferReader;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        //byte to char stream then reading char stream
+        System.out.println("You type: "+br.readLine());// working like scanner(System.in);
+        //from keyboard
     }
 }
