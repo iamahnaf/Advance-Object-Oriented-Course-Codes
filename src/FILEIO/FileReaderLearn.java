1ebd478c10c12331b2856f4ce9428f09c5bb391d
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-16 21:43:31 +0600

    update

---
+++ src/FILEIO/FileReaderLearn.java
@@ -0,0 +1,16 @@
+package FILEIO;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileReaderLearn {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("notes.txt");
+        int first = fr.read();
+        System.out.println(first);// FileReader returns -1 if there is no last digit
+        while (first != -1) {
+            System.out.print((char) first);
+            first = fr.read();
+        }
+    }
+}
