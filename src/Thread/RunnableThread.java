0f9dc8be795a9aa1542362258498b3c3ba35897f
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-17 21:42:53 +0600

    update

--- src/Thread/RunnableThread.java
+++ src/Thread/RunnableThread.java
@@ -1,10 +1,25 @@
 package Thread;
 class newThread implements Runnable {
+    Thread t; int name;
+    newThread(int name) {
+        t = new Thread(this,"runnable thread ");
+        this.name = name;
+        t.start();
+    }
+
     @Override
     public void run() {
+        //printing
+        for(int i=1;i<=5;i++){
+            System.out.println(name+"child thread"+i);
+
+        }
 
     }
 }
 public class RunnableThread  {
-
+    public static void main(String[] args) {
+        new newThread(1);
+        new newThread(2);
+    }
 }
