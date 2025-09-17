cae3e6b1620988407920110a449df0649fd492d9
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-17 21:05:43 +0600

    update

---
+++ src/Networking/Server.java
@@ -0,0 +1,38 @@
+package Networking;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+        //making server
+        ServerSocket serverSocket = new ServerSocket(5001);
+        System.out.println("Server started..");
+        while (true) {
+            //creating socket so it can handle a clinet
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected...");
+            //for comms need input and outputstream
+            ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
+
+            //for receiving need object input
+            try {
+                Object cmsg= objectInputStream.readObject();
+                System.out.println("For client: "+cmsg);
+                String serverMsg = (String) cmsg;
+                //making it uppcase
+                serverMsg=serverMsg.toUpperCase();
+                //sending it to client
+                objectOutputStream.writeObject(serverMsg);
+
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
+
+        }
+    }
+}
