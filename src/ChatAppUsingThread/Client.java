568225c0c8c3db8d3b18d85d5df8c33c25d69946
Ahnaf Tahmid <mtahmid2310203@bscse.uiu.ac.bd>
2025-09-18 15:36:26 +0600

    update

--- src/ChatAppUsingThread/Client.java
+++ src/ChatAppUsingThread/Client.java
@@ -9,23 +9,34 @@ public class Client {
     public static void main(String[] args) throws IOException {
         System.out.println("Client Started");
         //for connecting to server using ip and port
-        Socket socket = new Socket("localhost",5002);
+        Socket socket = new Socket("localhost",5001);
         System.out.println("Client connected");
+
         //now for comms object input and output stream
         ObjectOutputStream objectOutputStream = new ObjectOutputStream(socket.getOutputStream());
         ObjectInputStream objectInputStream = new ObjectInputStream(socket.getInputStream());
-        //for taking input from user for clinet msg
-        Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-        //sending msg to server
-        objectOutputStream.writeObject(input);
-        //receiving msg from server
-        try {
-            Object object=objectInputStream.readObject();
-            System.out.println("From server: "+(String) object);
-        } catch (ClassNotFoundException e) {
-            System.out.println(e.getMessage());
+
+
+        while (true) {
+            //for taking input from user for clinet msg
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if(input.equals("exit")){
+                break;
+            }
+
+            //sending msg to server
+            objectOutputStream.writeObject(input);
+
+            //receiving msg from server
+            try {
+                Object object=objectInputStream.readObject();
+                System.out.println("From server: "+(String) object);
+            } catch (ClassNotFoundException e) {
+                System.out.println(e.getMessage());
+            }
         }
+        socket.close();
     }
 }
 
